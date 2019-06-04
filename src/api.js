a48895fb10ffaece2ec1c6cce096f5641d3e913b
Leandre Blanchard <leandre.blanchard@parrot.com>
2019-06-04 18:33:54 +0200

    [DEV] CareApi stable version

--- src/api.js
+++ src/api.js
@@ -1,39 +1,48 @@
 import axios from 'axios';
 
+const userdataToken = 'epicare-login-data';
+
 export default class CareApi {
-    static tryRequest(usedMethod, data, npath) {
-        var response;
-        axios({
-            method: usedMethod,
-            url: 'http://localhost:8080/' + npath,
-            //url: 'http://user.epicare.fr/' + npath,
+    static async request(method, data, path) {
+        const response = await axios({
+            method: method,
+            url: path,
             data: data,
-        }).then(function(answer) {
-            console.log(answer);
-            response = answer.data;
-        }).catch(function(err) {
-            console.log(npath + ': ' + err);
+        }).catch(function (err) {
+            console.log(path + ': ' + err);
+            return null;
         });
         return response;
     }
+    static tryRequest(usedMethod, data, npath) {
+        return this.request(usedMethod, data, 'http://epicare.fr:8080/' + npath);
+    }
+    static tryQuery(method, query, npath) {
+        var url = npath + '?';
+
+        for (var key in query) {
+            url += key.toString() + '=' + query[key].toString() + '$';
+        }
+        url = url.slice(0, -1);
+        return this.tryRequest(method, undefined, url);
+    }
     static login(email, passwd) {
         const data = {
-            "login": email,
+            "email": email,
             "password": passwd
         };
         var content = this.tryRequest('POST', data, 'login');
-        console.log(content);
         return content;
     }
-    static register(name, lastName, email, passwd) {
+    static register(name, lastName, email, passwd, type) {
         const data = {
             "name": name,
             "last_name": lastName,
             "email": email,
-            "password": passwd
+            "password": passwd,
+            "type": type
         };
         var content = this.tryRequest('POST', data, 'register');
-        console.log(content);
         return content;
     }
     static confirmAccount(token) {
@@ -41,7 +50,6 @@ export default class CareApi {
             "token": token
         };
         var content = this.tryRequest('POST', data, 'confirm');
-        console.log(content);
         return content;
     }
     static sendResetPassword(email) {
@@ -49,7 +57,6 @@ export default class CareApi {
             "email": email
         };
         var content = this.tryRequest('GET', data, 'reset');
-        console.log(content);
         return content;
     }
     static resetPassword(emailToken, email, passwd) {
@@ -59,7 +66,33 @@ export default class CareApi {
             "password": passwd
         };
         var content = this.tryRequest('POST', data, 'reset');
-        console.log(content);
         return content;
+    }
+    static getProfileInfo() {
+        var token = this.getSessionToken();
+        if (token == null)
+            token = "";
+        const query = {
+            token: token,
+        };
+        var content = this.tryQuery('GET', query, 'profile');
+        return content;
+    }
+    static isConnected() {
+        var item = sessionStorage.getItem(userdataToken);
+        if (item === "undefined" || item == null)
+            return false;
+        return true;
+    }
+    static getSessionToken() {
+        var token = sessionStorage.getItem(userdataToken);
+        return token;
+    }
+    static setSessionToken(data) {
+        this.logout();
+        sessionStorage.setItem(userdataToken, data);
+    }
+    static logout() {
+        sessionStorage.clear();
     }
 }
