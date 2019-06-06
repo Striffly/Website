29927ef605064259de8a1932cf803aa1573a0e95
Tanguy D'auria$ <tanguy.d-auria@epitech.eu>
2019-06-06 23:17:13 +0200

    Changing in progress

---
+++ src/Components/Chat.js
@@ -0,0 +1,59 @@
+//import '../Public/Profile.css';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import CareApi from '../api';
+
+export default class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: null,
+        };
+        this.refreshInfo = (response) => {
+            if (response != null) {
+                console.log(response.data.user);
+                this.setState({
+                    user: response.data.user,
+                });
+            }
+        };
+    }
+    componentWillMount() {
+        CareApi.getProfileInfo().then(this.refreshInfo);
+    }
+    notLoggedPage() {
+        return (
+            <div className="center-screen">
+                <h1>You must be connected to access this page</h1>
+                <Link to="/login" className="btn btn-warning btn-space">Login</Link>
+            </div>
+        );
+    }
+    loadingScreenPage() {
+        return (
+            <div className="center-screen">
+                <h1>Loading ...</h1>
+            </div>
+        );
+    }
+    render() {
+        if (!CareApi.isConnected())
+            return this.notLoggedPage();
+        if (!this.state.user)
+            return this.loadingScreenPage();
+        return (
+            <div>
+                <div className="jumbotron">
+                    <h1 className="display-4">{this.state.user.name}</h1>
+                    <p className="lead">{this.state.user.last_name}</p>
+                    <p className="lead">{this.state.user.email}</p>
+                    <hr className="my-4" />
+                    <p>{this.state.user.type}</p>
+                    <p className="lead">
+                        <Link className="btn btn-lg btn-danger" to="/login" onClick={CareApi.logout}>Logout</Link>
+                    </p>
+                </div>
+            </div>
+        );
+    }
+}
--- src/Views/Chat.js
+++ src/Views/Chat.js
@@ -1,6 +1,7 @@
 import '../Public/Chat.css'
 import React, { Component } from 'react';
 import CareChat from '../Chat';
+import ChatView from '../Components/Chat'
 
 export default class Chat extends Component {
     constructor() {
@@ -27,17 +28,18 @@ export default class Chat extends Component {
     }
     render() {
         return (
-            <div className="container bg-faded box">
-                <br></br><br></br><br></br>
-                <div className="row list-group" id="messages" action="">
-                </div>
-                <div className="row input-group col-4 mx-auto chat-input">
-                    <input id="m" type="text" className="form-control" placeholder="Écrivez un message..." aria-label="Écrivez un message..." aria-describedby="basic-addon2" onSubmit={this.on_send.bind(this)}></input>
-                    <div className="input-group-append">
-                        <button className="btn btn-secondary" type="button" onClick={this.on_send.bind(this)}> Send </button>
-                    </div>
-                </div>
-            </div>
+          <ChatView />
+            // <div className="container bg-faded box">
+            //     <br></br><br></br><br></br>
+            //     <div className="row list-group" id="messages" action="">
+            //     </div>
+            //     <div className="row input-group col-4 mx-auto chat-input">
+            //         <input id="m" type="text" className="form-control" placeholder="Écrivez un message..." aria-label="Écrivez un message..." aria-describedby="basic-addon2" onSubmit={this.on_send.bind(this)}></input>
+            //         <div className="input-group-append">
+            //             <button className="btn btn-secondary" type="button" onClick={this.on_send.bind(this)}> Send </button>
+            //         </div>
+            //     </div>
+            // </div>
         )
     }
 }
