3ea299c787890da4b0f78bba91bf33fd2942681b
Prat <kentin.pratelli@epitech.eu>
2019-06-06 23:33:36 +0200

    not logged to prescription

--- src/Views/Prescription.js
+++ src/Views/Prescription.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
+import CareApi from "../api";
+import NotLogged from "../Components/NotLogged";
 
 export default class Prescription extends React.Component {
     render() {
+        if (!CareApi.isConnected())
+            return (<NotLogged/>);
         return (
             <Navbar/>
         )
