8f5703a33e143d16dd45f6db17798685e525bf33
Tanguy D'auria$ <tanguy.d-auria@epitech.eu>
2019-06-08 18:43:16 +0200

    Added basic html layout, next step : displaying messages

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,30 +1,39 @@
-//import '../Public/Profile.css';
+// import '../Public/Profile.css';
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import {
+  Container, Row, Col, Form, InputGroup, FormControl,
+} from 'react-bootstrap';
 import CareApi from '../api';
+import Navbar from './Navbar';
 
 export default class Login extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            user: null,
-        };
-        this.refreshInfo = (response) => {
-            if (response != null) {
-                console.log(response.data.user);
-                this.setState({
-                    user: response.data.user,
-                });
-            }
-        };
-    }
-    componentWillMount() {
-        CareApi.getProfileInfo().then(this.refreshInfo);
-    }
-    notLoggedPage() {
-        return (
-            <div className="center-screen">
-                <h1>You must be connected to access this page</h1>
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+      input: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.sendMessage = this.sendMessage.bind(this);
+    this.refreshInfo = (response) => {
+      if (response != null) {
+        console.log(response.data.user);
+        this.setState({
+          user: response.data.user,
+        });
+      }
+    };
+  }
+
+  componentWillMount() {
+    CareApi.getProfileInfo().then(this.refreshInfo);
+  }
+
+  notLoggedPage() {
+    return (
+          <div className="center-screen">
+              <h1>You must be connected to access this page</h1>
                 <Link to="/login" className="btn btn-warning btn-space">Login</Link>
             </div>
         );
@@ -36,24 +45,58 @@ export default class Login extends Component {
             </div>
         );
     }
-    render() {
-        if (!CareApi.isConnected())
-            return this.notLoggedPage();
-        if (!this.state.user)
-            return this.loadingScreenPage();
-        return (
-            <div>
-                <div className="jumbotron">
-                    <h1 className="display-4">{this.state.user.name}</h1>
-                    <p className="lead">{this.state.user.last_name}</p>
-                    <p className="lead">{this.state.user.email}</p>
-                    <hr className="my-4" />
-                    <p>{this.state.user.type}</p>
-                    <p className="lead">
-                        <Link className="btn btn-lg btn-danger" to="/login" onClick={CareApi.logout}>Logout</Link>
-                    </p>
-                </div>
-            </div>
-        );
+
+  handleChange(e) {
+   this.setState({ input: e.target.value });
+  }
+
+  sendMessage() {
+    alert(this.state.input);
+    this.setState({ input: '' });
+  }
+
+  render() {
+    if (!CareApi.isConnected()) {
+      return this.notLoggedPage();
     }
+    if (!this.state.user) {
+      return this.loadingScreenPage();
+    }
+    const displayName = `${this.state.user.last_name} ${this.state.user.name}`;
+    return (
+      <div>
+        <Navbar />
+        {
+        // <div className="jumbotron">
+        //   <h1 className="display-4">{this.state.user.name}</h1>
+        //   <p className="lead">{this.state.user.last_name}</p>
+        //   <p className="lead">{this.state.user.email}</p>
+        // </div>
+      }
+        <Container>
+          <Row style={{ border: '1px solid' }}>
+            <Col sm={1} style={{ paddingTop: '25px', paddingBottom: '25px' }}>
+              <img alt="avatar" style={{ width: '100%', height: 'auto' }} src={`${process.env.PUBLIC_URL}/user_default_avatar.png`} />
+            </Col>
+            <Col sm={2} style={{ paddingTop: '30px', paddingBottom: '25px' }}>{displayName}</Col>
+          </Row>
+          <Row style={{ borderLeft: '1px solid', borderRight: '1px solid', height: '70vh' }}>
+            <Col>
+              Coucou les amis
+            </Col>
+          </Row>
+          <Row style={{ border: '1px solid', height: '5vh' }}>
+            <Col sm={11}>
+              <FormControl style={{ height: '4vh', resize: 'none' }} value={this.state.input} as="textarea" aria-label="With textarea" placeholder="Envoyer un message" onChange={this.handleChange} />
+            </Col>
+            <Col sm={1}>
+              <button type="button" onClick={this.sendMessage}>
+                <i className="far fa-2x fa-paper-plane" />
+              </button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
