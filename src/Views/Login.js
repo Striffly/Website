7c25a93ee7ac9e1627f4da109c5b0d414a4b4be3
Tanguy D'auria$ <tanguy.d-auria@epitech.eu>
2019-06-11 23:03:05 +0200

    Added missing checks and inputs

--- src/Views/Login.js
+++ src/Views/Login.js
@@ -5,21 +5,6 @@ import {  withRouter } from 'react-router-dom';
 import { Button, Modal } from 'react-bootstrap';
 import CareApi from '../Api/api';
 
-function tryRegister() {
-    const name = document.getElementById('name-input').value;
-    const last_name = document.getElementById('last_name-input').value;
-    const email = document.getElementById('email-input').value;
-    const password = document.getElementById('password-input').value;
-    const type = document.getElementById('type-input').value;
-    var promise = CareApi.register(name, last_name, email, password, type);
-    promise.then(function(response) {
-        if (response != null && response.status === 200) {
-            alert("We've sent you a validation email");
-        }
-    });
-    // return false;
-}
-
 class Login extends Component {
     constructor(props) {
         super(props);
@@ -27,9 +12,10 @@ class Login extends Component {
         this.state = {
           isLogin: this.props.location.state,
           show: false,
-        }
+        };
         this.redirectPrescription = this.redirectPrescription.bind(this);
         this.tryLogin = this.tryLogin.bind(this);
+        this.tryRegister = this.tryRegister.bind(this);
         this.handleClose = this.handleClose.bind(this);
     }
     redirectPrescription(loginResponse) {
@@ -56,8 +42,41 @@ class Login extends Component {
         }
     }
 
+    tryRegister() {
+        const name = document.getElementById('name-input').value;
+        const last_name = document.getElementById('last_name-input').value;
+        const email = document.getElementById('email-input').value;
+        const password = document.getElementById('password-input').value;
+        const password_bis = document.getElementById('password-input-confirmation').value;
+        const date = document.getElementById('date-input').value;
+        const type = document.getElementById('type-input').value;
+        if (name === '' || last_name === '' || email === '' || password === '' || password_bis === '' || type === '' || date === '') {
+          this.errorMsg = "Erreur: un ou plusieurs champs n'ont pas été remplis.";
+          this.setState({ show: true });
+          return;
+        } else if (password_bis.localeCompare(password) !== 0) {
+          this.errorMsg = 'Erreur: veuillez entrer le même mot de passe.';
+          this.setState({ show: true });
+          return;
+        } else
+        var promise = CareApi.register(name, last_name, email, password, type);
+        promise.then(function(response) {
+            if (response != null && response.status === 200) {
+                alert("We've sent you a validation email");
+            } else {
+              this.errorMsg = 'Une erreur est survenue. Veuillez vérifier les informations saisies.';
+              this.setState({ show: true });
+            }
+        });
+        // return false;
+    }
+
     handleClose() {
       this.setState({ show: false });
+    }
+
+    handleChange(date) {
+       this.setState({ startDate: date });
     }
 
     render() {
@@ -99,6 +118,10 @@ class Login extends Component {
                               <input type="name" className="form-control" id="last_name-input" placeholder="Enter last name" />
                           </div>
                           <div className="form-group">
+                              <label>Birthdate</label>
+                              <input type="date" id="date-input" min="1900-01-10" max="2019-06-12" />
+                          </div>
+                          <div className="form-group">
                               <label>Email address</label>
                               <input type="email" className="form-control" id="email-input" placeholder="Enter email" />
                               <small className="form-text text-muted">We'll never share your email with anyone else.</small>
@@ -108,13 +131,25 @@ class Login extends Component {
                               <input type="password" className="form-control" id="password-input" placeholder="Password" />
                           </div>
                           <div className="form-group">
+                              <label>Password confirmation</label>
+                              <input type="password" className="form-control" id="password-input-confirmation" placeholder="Password" />
+                          </div>
+                          <div className="form-group">
                               <label>Account type</label>
                               <input type="name" className="form-control" id="type-input" placeholder="patient / doctor ?" />
                           </div>
-                          <button type="button" className="btn btn-block btn-primary" onClick={tryRegister}>Register</button>
+                          <button type="button" className="btn btn-block btn-primary" onClick={this.tryRegister}>Register</button>
                           <button className="btn btn-block btn-secondary btn-space" onClick={() => this.setState({ isLogin: true })}>Already registed ?</button>
                       </form>
                   </div>
+                  <Modal show={this.state.show} onHide={this.handleClose}>
+                    <Modal.Body>{this.errorMsg}</Modal.Body>
+                      <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handleClose}>
+                          Close
+                        </Button>
+                      </Modal.Footer>
+                    </Modal>
               </div>
             ) }
           </div>
