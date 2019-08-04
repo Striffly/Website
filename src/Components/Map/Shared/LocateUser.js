48267bbbdb129d72ded9a40656fbb60d8a462b22
Tanguy D'auria$ <tanguy.d-auria@epitech.eu>
2019-08-04 17:06:33 +0200

    Récupération de l'adresse

--- src/Components/Map/Shared/LocateUser.js
+++ src/Components/Map/Shared/LocateUser.js
@@ -1,17 +1,21 @@
 import { Component } from 'react';
 import { withLeaflet } from 'react-leaflet';
 import Locate from 'leaflet.locatecontrol';
+import * as ELG from 'esri-leaflet-geocoder';
 
 class LocateControl extends Component {
   componentDidMount() {
     const { startDirectly } = this.props;
     const { map } = this.props.leaflet;
+    const geocodeService = ELG.geocodeService();
     const locateOptions = {
       position: 'topright',
+      enableHighAccuracy: true,
       strings: {
-          title: 'Show me where I am, yo!'
+          title: 'Accéder à ma position',
       },
-      onActivate: () => {} // callback before engine starts retrieving locations
+      showCompass: false,
+      onActivate: () => { } // callback before engine starts retrieving locations
     }
 
     const lc = new Locate(locateOptions);
@@ -20,6 +24,16 @@ class LocateControl extends Component {
     if (startDirectly) {
       lc.start();
     }
+    map.on('locationerror', function(e){
+      alert("Location access denied.");
+    });
+    map.on('locationfound', function(e){
+      console.log(e.latlng);
+      geocodeService.reverse().latlng(e.latlng).run(function(error, result) {
+        console.log(result.latlng);
+        console.log(result.address.Match_addr);
+      });
+    });
   }
 
   render() {
