0fd46ce61956bde4d9d3dd01faa79d62800672c1
Camille ARBAULT <camille.arbault@epitech.eu>
2019-08-24 15:00:36 +0200

    mapRouting in react components (instead of js)

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -3,17 +3,13 @@ import {
     Map, TileLayer, withLeaflet, MapControl, ZoomControl,
 } from 'react-leaflet';
 import { GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
-import MapRouting from "./Map";
-
+import MapRouting from "./MapRouting";
 import LocateControl from './Shared/LocateUser';
 // import PrintControlDefault from 'react-leaflet-easyprint';
 // import MarkerClusterGroup from 'react-leaflet-markercluster';
 // import yellowIcon from './MarkerStyle';
-
-// import './customLeaflet.scss';
-//
+// import './customLeaflet.scss';//
 // import classes from '../GraphComponents.scss';
-
 // const PrintControl = withLeaflet(PrintControlDefault);
 
 class Search extends MapControl {
@@ -37,6 +33,7 @@ class LeafletMap extends Component {
         lat: 46.5,
         lng: 2.618787,
         zoom: 5,
+        isMapInit: false
     };
 
     // print() {
@@ -49,40 +46,22 @@ class LeafletMap extends Component {
 
     saveMap = (map) => {
         this.map = map;
-        this.setState({
-            isMapInit: false
-        })
     };
+
+    componentDidMount() {
+        console.log("map did mount " + this.map);
+        if (this.state.isMapInit === false) {
+            this.setState({
+                isMapInit: true
+            })
+        }
+    }
 
     render() {
 
         const position = [this.state.lat, this.state.lng];
         const hospitalTestRouting = [48.85412, 2.4065929];
-
-        // const printOptions = {
-        //   position: 'topleft',
-        //   sizeModes: ['Current', 'A4Portrait', 'A4Landscape'],
-        //   hideControlContainer: false
-        // };
-        // const downloadOptions = {
-        //   position: 'bottomright',
-        //   filename: this.props.filename,
-        //   sizeModes: ['Current', 'A4Portrait', 'A4Landscape'],
-        //   title: 'Télécharger au format PNG',
-        //   hideControlContainer: false,
-        //   exportOnly: true,
-        // };
-        // htmlToImage.toPng(document.getElementById('mapContainer'))
-        //   .then(function (dataUrl) {
-        //     download(dataUrl, 'my-node.png');
-        //   });
-
-        // alert(data[0].address[0].gps.lat);
-        // alert(data[0].address[0].gps.lon);
-
-        let mapRouting = null;
-        if (this.state.isMapInit)
-            mapRouting = <MapRouting routeFrom={position} routeTo={hospitalTestRouting} map={this.refs.map}/>;
+        const testCamillePos = [48.8552485,2.4344011];
 
         return (
             <Map
@@ -102,7 +81,7 @@ class LeafletMap extends Component {
                 <ZoomControl position="bottomleft" />
                 <GeoSearch />
                 <LocateControl startDirectly/>
-                {mapRouting}
+                {this.state.isMapInit && <MapRouting routeFrom={testCamillePos} routeTo={hospitalTestRouting} map={this.map}/>}
 
             </Map>
         );
---
+++ src/Components/Map/MapRouting.js
@@ -0,0 +1,34 @@
+import { MapLayer } from "react-leaflet";
+import L from "leaflet";
+import "leaflet-routing-machine";
+import { withLeaflet } from "react-leaflet";
+//import yellowIcon from './MarkerStyle';
+
+class MapRouting extends MapLayer {
+
+    createLeafletElement() {
+
+        const { map, routeFrom, routeTo} = this.props;
+        const waypoints = [routeFrom, routeTo];
+        const icon = L.icon({
+            iconUrl:
+                "https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png",
+            iconSize: [40, 40]
+        });
+
+        let leafletElement = L.Routing.control({
+            waypoints,
+            plan: L.Routing.plan(waypoints, {
+                createMarker: function(i, wp) {
+                    return L.marker(wp.latLng, {
+                        draggable: true,
+                        icon
+                    });
+                },
+                routeWhileDragging: true
+            })
+        }).addTo(map.leafletElement);
+        return leafletElement.getPlan();
+    }
+}
+export default withLeaflet(MapRouting);
