92e4471d3aa3b497e1c0e35b7f740e3cbd4e3650
Tanguy D'auria$ <tanguy.d-auria@epitech.eu>
2019-09-03 18:01:45 -0400

    Ajout d'un marqueur permanent à l'emplacement choisi par l'utilisateur

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import {
-  Map, TileLayer, withLeaflet, MapControl, ZoomControl,
+  Map, TileLayer, ZoomControl, Marker
 } from 'react-leaflet';
 // import { GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
 
 import LocateControl from './Shared/LocateUser';
-import DiscreteSlider from './Shared/Slider';
+// import DiscreteSlider from './Shared/Slider';
 import Search from './Shared/Search';
 // import PrintControlDefault from 'react-leaflet-easyprint';
 // import MarkerClusterGroup from 'react-leaflet-markercluster';
@@ -18,11 +18,25 @@ import Search from './Shared/Search';
 // const PrintControl = withLeaflet(PrintControlDefault);
 
 class LeafletMap extends Component {
+  constructor(props) {
+    super(props);
+    this.mapRef = React.createRef();
+    this.state = {
+        lat: 46.5,
+        lng: 2.618787,
+        markerLat: 0,
+        markerLng: 0,
+        zoom: 5,
+      }
+    this.addMarker = this.addMarker.bind(this);
+  }
 
-  state = {
-    lat: 46.5,
-    lng: 2.618787,
-    zoom: 5,
+  addMarker(x, y) {
+    // alert('coucou');
+    // alert(x);
+    // alert(y);
+    console.log("X vaut " + x + " et y vaut " + y);
+    this.setState({ markerLat: x, markerLng: y });
   }
 
   // print() {
@@ -35,6 +49,13 @@ class LeafletMap extends Component {
 
   render() {
     const position = [this.state.lat, this.state.lng];
+    // if (this.mapRef.current != null) {
+    //   this.addMarker(50, 50);
+    //   // map.on('geosearch/showlocation', function(param) {
+    //   //   this.props.addMarker(param.location.x, param.location.y);
+    //   // });
+    // }
+    // console.log(this.mapRef);
     // map.on('geosearch/showlocation', function() {
     //   alert('toto');
     // });
@@ -61,13 +82,22 @@ class LeafletMap extends Component {
 
     return (
       <div>
-        <Map zoomControl={false} center={position} zoom={this.state.zoom} style={{ height: '100vh' }} minZoom={2} maxZoom={19}>
+        <Map
+          zoomControl={false}
+          center={position}
+          zoom={this.state.zoom}
+          style={{ height: '100vh' }}
+          minZoom={2}
+          max Zoom={19}
+          ref={this.mapRef}
+        >
           <TileLayer
             attribution='&amp;copy <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &amp;copy <a href="https://carto.com/attributions">CARTO</a>'
             url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
           />
+          <Marker position={[this.state.markerLat, this.state.markerLng]} />
           <ZoomControl position="bottomleft" />
-          <Search />
+          <Search addMarker={this.addMarker}/>
           <LocateControl startDirectly/>
         </Map>
       </div>
--- src/Components/Map/Shared/Search.js
+++ src/Components/Map/Shared/Search.js
@@ -1,9 +1,14 @@
-import React from 'react';
 import { withLeaflet, MapControl } from 'react-leaflet';
 import { GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
 
 class Search extends MapControl {
   createLeafletElement() {
+    const map = this.props.leaflet.map;
+    // console.log(this.props);
+    // alert('toto');
+    map.on('geosearch/showlocation', (param)  => {
+      this.props.addMarker(param.location.y, param.location.x);
+    });
     return GeoSearchControl({
       // style: 'button',
       autoClose: true,
