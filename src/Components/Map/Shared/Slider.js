30197e8f7ac3d7625d73f7523e0a9de1b6562dd9
Tanguy D'auria$ <tanguy.d-auria@epitech.eu>
2019-09-07 19:13:27 -0400

    Fixed warning

--- src/Components/Map/Shared/Slider.js
+++ src/Components/Map/Shared/Slider.js
@@ -1,58 +1,58 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Slider from '@material-ui/core/Slider';
+// import React from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
+// import Typography from '@material-ui/core/Typography';
+// import Slider from '@material-ui/core/Slider';
 import { withLeaflet, MapControl } from "react-leaflet";
 import L from "leaflet";
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    width: 300,
-  },
-  margin: {
-    height: theme.spacing(3),
-  },
-}));
-
-const marks = [
-  {
-    value: 0,
-    label: '10km',
-  },
-  {
-    value: 100,
-    label: '100km',
-  }
-];
-
-function valuetext(value) {
-  return `${value}°C`;
-}
-
-function valueLabelFormat(value) {
-  return marks.findIndex(mark => mark.value === value) + 1;
-}
-
-function DiscreteSlider() {
-  const classes = useStyles();
-
-  return (
-    <div className={classes.root} >
-      <Typography id="discrete-slider" gutterBottom>
-        Rayon de la recherche
-      </Typography>
-      <Slider
-        defaultValue={0}
-        valueLabelFormat={valueLabelFormat}
-        getAriaValueText={valuetext}
-        aria-labelledby="discrete-slider-restrict"
-        step={null}
-        valueLabelDisplay="auto"
-        marks={marks}
-      />
-    </div>
-  );
-}
+// const useStyles = makeStyles(theme => ({
+//   root: {
+//     width: 300,
+//   },
+//   margin: {
+//     height: theme.spacing(3),
+//   },
+// }));
+//
+// const marks = [
+//   {
+//     value: 0,
+//     label: '10km',
+//   },
+//   {
+//     value: 100,
+//     label: '100km',
+//   }
+// ];
+//
+// function valuetext(value) {
+//   return `${value}°C`;
+// }
+//
+// function valueLabelFormat(value) {
+//   return marks.findIndex(mark => mark.value === value) + 1;
+// }
+//
+// function DiscreteSlider() {
+//   const classes = useStyles();
+//
+//   return (
+//     <div className={classes.root} >
+//       <Typography id="discrete-slider" gutterBottom>
+//         Rayon de la recherche
+//       </Typography>
+//       <Slider
+//         defaultValue={0}
+//         valueLabelFormat={valueLabelFormat}
+//         getAriaValueText={valuetext}
+//         aria-labelledby="discrete-slider-restrict"
+//         step={null}
+//         valueLabelDisplay="auto"
+//         marks={marks}
+//       />
+//     </div>
+//   );
+// }
 
 class MapInfo extends MapControl {
 
