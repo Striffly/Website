d5ea51c455c02cb96666999ba0e0908078b28048
Camille ARBAULT <camille.arbault@epitech.eu>
2019-09-09 12:39:18 +0200

    update webpack dev config to fix styling

--- src/Components/Map/MapRouting.js
+++ src/Components/Map/MapRouting.js
@@ -2,7 +2,9 @@ import { MapLayer } from "react-leaflet";
 import L from "leaflet";
 import "leaflet-routing-machine";
 import { withLeaflet } from "react-leaflet";
-import './leafletRoutingMachine.scss';
+//import './leafletRoutingMachine.scss'; override css with custom css
+import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
+
 
 class MapRouting extends MapLayer {
 
@@ -15,7 +17,7 @@ class MapRouting extends MapLayer {
             waypoints,
             showAlternatives: true,
             lineOptions: {
-                styles: [{color: 'red', opacity: 0.5, weight: 4 }]
+                styles: [{color: 'red', opacity: 0.5, weight: 6 }]
             },
             altLineOptions: {
                 styles: [{color: '#000', opacity: 0.4, weight: 4}] },
