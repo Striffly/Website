a8b3a673583eb6d00628003638408ee86190c5d5
Tanguy D'auria$ <tanguy.d-auria@epitech.eu>
2019-10-06 15:33:03 -0400

    changed api url

--- src/Components/Shared/Api/api.js
+++ src/Components/Shared/Api/api.js
@@ -16,7 +16,7 @@ export default class KwiliApi  {
     }
 
     static tryRequest(usedMethod, data, npath) {
-        return this.request(usedMethod, data, 'http://api.kwili.fr:8080/' + npath);
+        return this.request(usedMethod, data, 'https://kwili.fr:8080/' + npath);
     }
 
     static tryQuery(method, query, npath) {
